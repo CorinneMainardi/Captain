9118e4870bf9247312550c4f1f89d4df4db4d5b5
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-11-08 19:56:33 +0100

    fixedv style

--- src/app/pages/user-detail/user-detail.module.ts
+++ src/app/pages/user-detail/user-detail.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserDetailRoutingModule } from './user-detail-routing.module';
 import { UserDetailComponent } from './user-detail.component';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [UserDetailComponent],
-  imports: [CommonModule, UserDetailRoutingModule, NzCardModule],
+  imports: [CommonModule, UserDetailRoutingModule, NzCardModule, NzIconModule],
 })
 export class UserDetailModule {}
